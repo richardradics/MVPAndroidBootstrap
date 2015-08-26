cef2a0cafe534e99a5994b3192f93fe8f963dfdb
IgorGanapolsky <eazyigz@gmail.com>
2015-08-26 11:53:41 -0400

    Update gradle versions and dependencies. Fix RecyclerItemClickListener.

--- app/src/main/java/com/richardradics/cleanaa/util/RecyclerItemClickListener.java
+++ app/src/main/java/com/richardradics/cleanaa/util/RecyclerItemClickListener.java
@@ -41,4 +41,8 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
     @Override
     public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) {
     }
+
+    @Override public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
 }
